8f2d3eed4a51894fb404e6c9b984a7ba55085eda
Divya.Inapakurthi <87583059+Divya6265@users.noreply.github.com>
2023-07-14 11:07:50 -0400

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+// connecting to atlas mongoDB 
+const mongoose=require('mongoose')
+// this is the link provide by atlas to establish connection with cloud DB and...
+// provide aproppriate username and passwrod details along the DB want to connect. 
+//mongoose.connect('mongodb+srv://username:passwd@cluster0.pobbk4f.mongodb.net/TODO');
+
+mongoose.connect('mongodb+srv://21505a0506:Divya%40123@cluster0.pobbk4f.mongodb.net/TODO');
+const db=mongoose.connection;
+db.on('error',console.error.bind(console,'error in connection'));
+db.once('open',function(){
+    console.log("DB CONNECTION SUCCESSFULL")
+})
+
+
