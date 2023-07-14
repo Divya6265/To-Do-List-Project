b03683424e00638497cad63ffc9d360ffb12cbb6
Divya.Inapakurthi <87583059+Divya6265@users.noreply.github.com>
2023-07-14 14:02:07 -0400

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 // connecting to atlas mongoDB 
 const mongoose=require('mongoose')
-// this is the link provide by atlas to establish connection with cloud DB and...
-// provide aproppriate username and passwrod details along the DB want to connect. 
+// this is a link provide by atlas to establish connection with cloud DB and...
+// provide appropriate username and passwrod details along with the DB that you want to connect. 
 //mongoose.connect('mongodb+srv://username:passwd@cluster0.pobbk4f.mongodb.net/TODO');
 
 mongoose.connect('mongodb+srv://21505a0506:Divya%401@cluster0.pobbk4f.mongodb.net/TODO');
