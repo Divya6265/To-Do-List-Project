c64f0c728d96c8b8c17f0d93da5f1145076720c5
Divya.Inapakurthi <87583059+Divya6265@users.noreply.github.com>
2023-07-14 13:58:55 -0400

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose')
 // provide aproppriate username and passwrod details along the DB want to connect. 
 //mongoose.connect('mongodb+srv://username:passwd@cluster0.pobbk4f.mongodb.net/TODO');
 
-mongoose.connect('mongodb+srv://21505a0506:Divya%40123@cluster0.pobbk4f.mongodb.net/TODO');
+mongoose.connect('mongodb+srv://21505a0506:Divya%401@cluster0.pobbk4f.mongodb.net/TODO');
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error in connection'));
 db.once('open',function(){
